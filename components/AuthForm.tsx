19c9121cad169d52dc19ba0489ef65b69316e01a
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-09-18 10:46:17 -0500

    Started servernactions and finished auth forms.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,8 +19,10 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 const AuthForm = ({type}: {type: string}) => {
+    const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -35,12 +37,31 @@ const AuthForm = ({type}: {type: string}) => {
     })
      
       // 2. Define a submit handler.
-    const onSubmit = async(values: z.infer<typeof formSchema>) =>{
+    const onSubmit = async(data: z.infer<typeof formSchema>) => {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         setIsLoading(true);
-        console.log(values)
-        setIsLoading(false);
+        try {
+            if(type === "sign-up") {
+                const newUser = await signUp(data);
+                setUser(newUser);
+            }
+
+            if(type === "sign-in") {
+                const response = await signIn({
+                    email: data.email,
+                    password: data.password
+                });
+
+                if(response) {
+                    router.push("/");
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false);
+        }
     }
 
   return (
@@ -77,13 +98,20 @@ const AuthForm = ({type}: {type: string}) => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         {type === "sign-up" && (
                             <>
-                                <CustomInput control={form.control} name="firstName" label="First Name" placeholder="Enter your first name" />
-                                <CustomInput control={form.control} name="lastName" label="Last Name" placeholder="Enter your last name" />
+                                <div className='flex gap-4'>
+                                    <CustomInput control={form.control} name="firstName" label="First Name" placeholder="Enter your first name" />
+                                    <CustomInput control={form.control} name="lastName" label="Last Name" placeholder="Enter your last name" />
+                                </div>
                                 <CustomInput control={form.control} name="address1" label="Address" placeholder="Enter your specific address" />
-                                <CustomInput control={form.control} name="state" label="State" placeholder="Ex: OH" />
-                                <CustomInput control={form.control} name="postalCode" label="Postal Code" placeholder="Ex: 11022" />
-                                <CustomInput control={form.control} name="dateOfBirth" label="Date of Birth" placeholder="yyyy-mm-dd" />
-                                <CustomInput control={form.control} name="ssn" label="SSN" placeholder="Ex: 1234" />
+                                <CustomInput control={form.control} name="city" label="City" placeholder="Enter your city" />
+                                <div className='flex gap-4'>
+                                    <CustomInput control={form.control} name="state" label="State" placeholder="Ex: OH" />
+                                    <CustomInput control={form.control} name="postalCode" label="Postal Code" placeholder="Ex: 11022" />
+                                </div>
+                                <div className='flex gap-4'>
+                                    <CustomInput control={form.control} name="dateOfBirth" label="Date of Birth" placeholder="yyyy-mm-dd" />
+                                    <CustomInput control={form.control} name="ssn" label="SSN" placeholder="Ex: 1234" />
+                                </div>
                                 
                             </>
                         )}
