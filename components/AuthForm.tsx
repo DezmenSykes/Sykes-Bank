3b31afabd27f4ffc004e6bcfb2e8c7ba3d774a10
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-09-18 11:57:29 -0500

    Implemented user signup and session initialization using appwrite.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,6 +20,7 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import { signIn, signUp } from '@/lib/actions/user.actions'
 
 const AuthForm = ({type}: {type: string}) => {
     const router = useRouter();
@@ -48,14 +49,14 @@ const AuthForm = ({type}: {type: string}) => {
             }
 
             if(type === "sign-in") {
-                const response = await signIn({
-                    email: data.email,
-                    password: data.password
-                });
+                // const response = await signIn({
+                //     email: data.email,
+                //     password: data.password
+                // });
 
-                if(response) {
-                    router.push("/");
-                }
+                // if(response) {
+                //     router.push("/");
+                // }
             }
         } catch (error) {
             console.log(error)
