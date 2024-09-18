4ab36c598516ba9dc4233a3907f43facfabb864a
Dezmen Sykes <dezmensykes.ds1@gmail.com>
2024-09-18 10:26:45 -0500

    Added extedned feilds for sign-up form .

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,12 +18,16 @@ import {
 import { Input } from "@/components/ui/input"  
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
 
 const AuthForm = ({type}: {type: string}) => {
     const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const form = useForm<z.infer<typeof authFormSchema>>({
-        resolver: zodResolver(authFormSchema),
+    const formSchema = authFormSchema(type);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
           email: "",
           password: "",
@@ -31,10 +35,12 @@ const AuthForm = ({type}: {type: string}) => {
     })
      
       // 2. Define a submit handler.
-    const onSubmit = async(values: z.infer<typeof authFormSchema>) =>{
+    const onSubmit = async(values: z.infer<typeof formSchema>) =>{
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
+        setIsLoading(true);
         console.log(values)
+        setIsLoading(false);
     }
 
   return (
@@ -69,11 +75,39 @@ const AuthForm = ({type}: {type: string}) => {
             <>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        {type === "sign-up" && (
+                            <>
+                                <CustomInput control={form.control} name="firstName" label="First Name" placeholder="Enter your first name" />
+                                <CustomInput control={form.control} name="lastName" label="Last Name" placeholder="Enter your last name" />
+                                <CustomInput control={form.control} name="address1" label="Address" placeholder="Enter your specific address" />
+                                <CustomInput control={form.control} name="state" label="State" placeholder="Ex: OH" />
+                                <CustomInput control={form.control} name="postalCode" label="Postal Code" placeholder="Ex: 11022" />
+                                <CustomInput control={form.control} name="dateOfBirth" label="Date of Birth" placeholder="yyyy-mm-dd" />
+                                <CustomInput control={form.control} name="ssn" label="SSN" placeholder="Ex: 1234" />
+                                
+                            </>
+                        )}
                         <CustomInput control={form.control} name="email" label="Email" placeholder="Enter your email" />
                         <CustomInput control={form.control} name="password" label="Password" placeholder="Enter your password" />
-                        <Button type="submit">Submit</Button>
+                        <div className='flex flex-col gap-4'>
+                            <Button className='form-btn' type="submit" disabled={isLoading}>
+                                {isLoading ? (
+                                    <>
+                                        <Loader2 size={20} className="animate-spin" /> &nbsp;
+                                        Loading...
+                                    </>
+                                ): type === "sign-in" ? "Sign In" : "Sign Up"}
+                            </Button>
+                        </div>
+                        
                     </form>
                 </Form>
+
+
+                <footer className='flex justify-center gap-1'>
+                    <p className='text-14 font-normal text-gray-600 '>{type === "sign-in" ? "Don't have an account?" : "Already have an account?"}</p>
+                    <Link href={type === 'sign-in'? '/sign-up' : '/sign-in'} className='form-link'>{type === 'sign-in'? 'Sign Up' : 'Sign In'}</Link>
+                </footer>
             </>
         )}
     </section>
--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
 import { Input } from './ui/input'
-import { Control, Form } from 'react-hook-form'
+import { Control, FieldPath, Form } from 'react-hook-form'
 import { z } from 'zod'
 import { authFormSchema } from '@/lib/utils'
 
+const formSchema = authFormSchema("sign-up");
+
 interface CustomInput {
-    control: Control<z  .infer<typeof authFormSchema>, any>,
-    name: 'email' | 'password',
+    control: Control<z  .infer<typeof formSchema>, any>,
+    name: FieldPath<z.infer<typeof formSchema>>,
     label: string,
     placeholder: string,
-
 }
 
 const CustomInput = ({control, name, label, placeholder}: CustomInput) => {
@@ -25,7 +26,7 @@ const CustomInput = ({control, name, label, placeholder}: CustomInput) => {
                     </FormLabel>
                     <div className='flex w-full flex-col'>
                         <FormControl>
-                            <Input className='input-class' placeholder={placeholder} {...field} />
+                            <Input className='input-class' type={name === "password" ? "password" : "text"} placeholder={placeholder} {...field} />
                         </FormControl>
                         <FormMessage className='form-message mt-2' />
                     </div>
